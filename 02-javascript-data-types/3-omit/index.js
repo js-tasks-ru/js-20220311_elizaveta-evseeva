0e80fc9578f776d472ad815f7245687bb1fa60ba
Elizaveta Evseeva <elizavetaevseeva@192-168-0-104.local>
2022-03-18 01:55:29 +0300

    Solution of the task for the 2d lesson: omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -6,4 +6,14 @@
  */
 export const omit = (obj, ...fields) => {
 
+    const entries = Object.entries(obj);
+    const newEntries = new Map();
+    
+    for (let i = 0; i < entries.length; i++) {
+        if (!fields.includes(entries[i][0])) {
+            newEntries.set(entries[i][0], entries[i][1]);        
+        }
+    }
+
+    return Object.fromEntries(newEntries);
 };
